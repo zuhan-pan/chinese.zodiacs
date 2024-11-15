88a609508db9df7d125662d439c4a31e946d4bc4
zuhan <harrypan111@gmail.com>
2024-11-15 01:04:33 +0000

    add for responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //query selectors
 const imgSlider = document.querySelector('.img-slider');
-const nextBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
 const allItems = document.querySelectorAll('.img-item');
 const allInfos = document.querySelectorAll('.info-item');
 const carousel = document.querySelector('.carousel');
@@ -19,6 +17,12 @@ const allBackgrounds = [
   'gou',
   'zhu',
 ];
+let nextBtn = document.querySelector(
+  '.img-item.active .item .navigation .next-btn'
+);
+let prevBtn = document.querySelector(
+  '.img-item.active .item .navigation .prev-btn'
+);
 
 //evnet listners
 nextBtn.addEventListener('click', toNextSlide);
@@ -49,6 +53,14 @@ function toNextImage() {
     if (i % 12 === 2 || i % 12 === -10) {
       el.classList.add('active');
       el.style.setProperty('--i', `${i - 1}`);
+      nextBtn = document.querySelector(
+        '.img-item.active .item .navigation .next-btn'
+      );
+      prevBtn = document.querySelector(
+        '.img-item.active .item .navigation .prev-btn'
+      );
+      nextBtn.addEventListener('click', toNextSlide);
+      prevBtn.addEventListener('click', toPrevSlide);
     } else {
       el.style.setProperty('--i', `${i - 1}`);
     }
@@ -62,6 +74,14 @@ function toPrevImage() {
     if (i % 12 === 0) {
       el.classList.add('active');
       el.style.setProperty('--i', `${i + 1}`);
+      nextBtn = document.querySelector(
+        '.img-item.active .item .navigation .next-btn'
+      );
+      prevBtn = document.querySelector(
+        '.img-item.active .item .navigation .prev-btn'
+      );
+      nextBtn.addEventListener('click', toNextSlide);
+      prevBtn.addEventListener('click', toPrevSlide);
     } else {
       el.style.setProperty('--i', `${i + 1}`);
     }
